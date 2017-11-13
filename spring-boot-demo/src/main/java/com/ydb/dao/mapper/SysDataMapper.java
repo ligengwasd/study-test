5c7b2630fa1b33ce51f812f56f7204603ddc7f6e
李赓 <ligeng@1dabang.cn>
2017-11-13 21:05:35 +0800

    update

--- spring-boot-demo/src/main/java/com/ydb/dao/mapper/SysDataMapper.java
+++ spring-boot-demo/src/main/java/com/ydb/dao/mapper/SysDataMapper.java
@@ -1,6 +1,8 @@
 package com.ydb.dao.mapper;
 
 import org.apache.ibatis.annotations.Update;
+import org.springframework.data.jpa.repository.query.Procedure;
+
 /**
  * SysDataMapper:
  *
@@ -10,4 +12,7 @@ public interface SysDataMapper {
 
     @Update("UPDATE sys_data SET `level` = `level` -1 WHERE id = 1 and level>0")
     int testSecKill();
+
+    @Procedure()
+    int executeSecKill();
 }
---
+++ spring-boot-demo/src/main/java/com/ydb/entity/mysql/DrugBaseEntity.java
@@ -0,0 +1,24 @@
+package com.ydb.entity.mysql;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Created by ligeng on 17/9/30.
+ */
+@Data
+@MappedSuperclass
+public class DrugBaseEntity extends BaseEntity{
+    @ApiModelProperty("创建人")
+    private Long createUser;
+
+    @ApiModelProperty("更新人")
+    private Long updateUser;
+
+    @ApiModelProperty("状态 1:删除 0：正常")
+    private int status =0;
+}
