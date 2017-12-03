63efb5bf2504dd3bcd39b42b63f74edad9c0e818
李赓 <ligeng@1dabang.cn>
2017-12-04 01:22:19 +0800

    spring redis 分布式缓存

--- spring-boot-demo/src/main/java/com/ydb/dao/mapper/SysDataMapper.java
+++ spring-boot-demo/src/main/java/com/ydb/dao/mapper/SysDataMapper.java
@@ -3,6 +3,7 @@ package com.ydb.dao.mapper;
 import com.ydb.entity.mysql.SysData;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.query.Procedure;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.query.Procedure;
  * @author linwei
  */
 public interface SysDataMapper {
+    @Cacheable("sysDataCache")
     SysData findById(@Param("id") long id);
 
 //    @Update("UPDATE sys_data SET `level` = `level` -1 WHERE id = 1 and level>0")
