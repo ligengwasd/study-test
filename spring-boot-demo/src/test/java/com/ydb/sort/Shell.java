866dc7faa9f819fbbd58c7b3d1d5138cf97dfdcb
李赓 <ligeng@1dabang.cn>
2017-12-13 20:05:57 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/sort/Shell.java
+++ spring-boot-demo/src/test/java/com/ydb/sort/Shell.java
@@ -5,7 +5,7 @@ package com.ydb.sort;
  */
 public class Shell {
     public static void main(String[] args) {
-        int[] s = {6,1,2,7,9,3,4,5,10,8};
+        int[] s = {6,1,2,7,9,3,4,5,10,8,20};
         Util.print(s);
         sort(s);
     }
@@ -14,7 +14,7 @@ public class Shell {
         int gap,i,j;
         for (gap=s.length/2; gap>0; gap/=2){
             for (i=gap; i<s.length; i++){
-                for (j=i; j>0 && s[j-1]>s[j]; j--){
+                for (j=i; j>0 && s[j-gap]>s[j]; j-=gap){
                     Util.swap(s, j-1,j);
                     Util.print(s);
                 }
