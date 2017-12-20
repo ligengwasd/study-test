043b44b5c9c7f9c9f479c130256b7a9320f27646
李赓 <ligeng@1dabang.cn>
2017-12-20 19:50:11 +0800

    update

---
+++ tcc-zuul/src/main/java/com/flint/tcc/zuul/TccZuulApplication.java
@@ -0,0 +1,12 @@
+package com.flint.tcc.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccZuulApplication.class, args);
+	}
+}
