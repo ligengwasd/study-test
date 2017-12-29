8d84ac7e8a104779b5bed9ee607b752bbbc9f4c3
李赓 <ligeng@1dabang.cn>
2017-12-29 19:02:00 +0800

    update

---
+++ spring-boot-elasticsearch/src/main/java/com/flint/elasticsearch/data/mongo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by ligeng on 17/12/29.
+ */
+package com.flint.elasticsearch.data.mongo;
--- spring-boot-elasticsearch/src/main/java/com/flint/elasticsearch/data/mysql/package-info.java
+++ spring-boot-elasticsearch/src/main/java/com/flint/elasticsearch/data/mysql/package-info.java
@@ -1,5 +1,5 @@
 /**
- * msyql entity repository mapper
+ * mysql entity repository mapper
  * Created by ligeng on 17/12/29.
  */
 package com.flint.elasticsearch.data.mysql;
