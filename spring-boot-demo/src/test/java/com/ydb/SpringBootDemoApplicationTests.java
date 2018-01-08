4c7ba45317d906fb71cec096da24357e7a8b846e
李赓 <ligeng@1dabang.cn>
2018-01-08 13:39:55 +0800

    测试mysql性能

--- spring-boot-demo/src/test/java/com/ydb/SpringBootDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/com/ydb/SpringBootDemoApplicationTests.java
@@ -1,14 +1,19 @@
 package com.ydb;
 
 import com.ydb.dao.repository.SysDataRepository;
+import com.ydb.entity.mysql.SysData;
+import org.apache.commons.lang.math.RandomUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,5 +26,20 @@ public class SpringBootDemoApplicationTests {
 		int i = sysDataRepository.executeSeckill(1000l,15121128676l, new Date());
 		System.out.println(i);
 	}
+	@Test
+	public void test2() {
+		List<SysData> data = new ArrayList<>();
+		for (int i=1; i<=100000; i++) {
+			data.clear();
+			for (int j=1; j<=10000; j++) {
+				SysData temp =new SysData();
+				temp.setLevel(RandomUtils.nextInt(3000000));// user id
+				temp.setOrderNo(RandomUtils.nextInt(800000));// content id
+				data.add(temp);
+			}
+			sysDataRepository.save(data);
+		}
+	}
+
 
 }
