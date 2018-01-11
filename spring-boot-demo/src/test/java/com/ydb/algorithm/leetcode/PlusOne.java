e4e9e7f00d83120703655d018dfd51397f6a4072
李赓 <ligeng@1dabang.cn>
2018-01-11 19:47:57 +0800

    plus one

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/PlusOne.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/PlusOne.java
@@ -6,29 +6,23 @@ package com.ydb.algorithm.leetcode;
 public class PlusOne {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] s = solution.plusOne(new int[]{1,2,9});
+        int[] s = solution.plusOne(new int[]{9,9,9});
         System.out.println(s);
     }
 
     static class Solution {
         public int[] plusOne(int[] digits) {
-            int c=0;
-            for(int i=digits.length-1;i>=0;i--){
-                digits[i]++;
-                c=digits[i]/10;
-                if(c==0)
-                    break;
-                digits[i]%=10;
+            int n = digits.length;
+            for (int i = digits.length - 1; i >= 0; --i) {
+                if (digits[i] < 9) {
+                    ++digits[i];
+                    return digits;
+                }
+                digits[i] = 0;
             }
-            System.out.println("c:"+c);
-            if(c==0)
-                return digits;
-            int[] newdigits=new int[digits.length+1];
-            newdigits[0]=1;
-            for(int i=0;i<digits.length;i++)
-                newdigits[i+1]=digits[i];
-            return newdigits;
-
+            int[] res = new int[n + 1];
+            res[0] = 1;
+            return res;
         }
     }
 }
