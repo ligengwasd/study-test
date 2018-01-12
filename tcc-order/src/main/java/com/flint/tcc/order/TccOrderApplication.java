c705d8fae457e69c1799d1a229b41430c21942e3
李赓 <ligeng@1dabang.cn>
2018-01-12 11:35:33 +0800

    update

--- tcc-order/src/main/java/com/flint/tcc/order/TccOrderApplication.java
+++ tcc-order/src/main/java/com/flint/tcc/order/TccOrderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +27,7 @@ import java.util.Set;
 @ImportResource(locations = "classpath:tcc-transaction.xml")
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class TccOrderApplication {
 
 	public static void main(String[] args) {
