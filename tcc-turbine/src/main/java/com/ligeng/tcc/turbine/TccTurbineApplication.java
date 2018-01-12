ca8b5c9e4403af518926fe08952f6a8410f1104f
李赓 <ligeng@1dabang.cn>
2018-01-12 13:16:03 +0800

    update

---
+++ tcc-turbine/src/main/java/com/ligeng/tcc/turbine/TccTurbineApplication.java
@@ -0,0 +1,12 @@
+package com.ligeng.tcc.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccTurbineApplication.class, args);
+	}
+}
