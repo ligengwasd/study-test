1a7efa0473335f1fddbc469a48f8e1e498e3ae5b
李赓 <ligeng@1dabang.cn>
2018-01-13 11:10:25 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
@@ -10,33 +10,34 @@ public class SearchInsert {
         Solution solution = new Solution();
         System.out.println(solution.searchInsert(nums,val));
     }
-}
 
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        if (nums[0] > target) return 0;
-        if (nums[nums.length-1] < target) return nums.length;
-        return searchInsertRecursion(nums,0,nums.length-1,target);
-    }
+    static class Solution {
+        public int searchInsert(int[] nums, int target) {
+            if (nums[0] > target) return 0;
+            if (nums[nums.length-1] < target) return nums.length;
+            return searchInsertRecursion(nums,0,nums.length-1,target);
+        }
 
-    private int searchInsertRecursion(int[] nums,int left,int right,int target){
-        if (nums[left] == target) {
-            return left;
-        } else if (nums[right] == target){
-            return right;
-        } else if (nums[left] < target && target < nums[right]){
-            if (right - left == 1) {
-                return left+1;
-            } else {
-                int mid = (left+right+1)/2;
-                if (target <= nums[mid]) {
-                    return searchInsertRecursion(nums, left, mid, target);
+        private int searchInsertRecursion(int[] nums,int left,int right,int target){
+            if (nums[left] == target) {
+                return left;
+            } else if (nums[right] == target){
+                return right;
+            } else if (nums[left] < target && target < nums[right]){
+                if (right - left == 1) {
+                    return left+1;
                 } else {
-                    return searchInsertRecursion(nums, mid, right, target);
+                    int mid = (left+right+1)/2;
+                    if (target <= nums[mid]) {
+                        return searchInsertRecursion(nums, left, mid, target);
+                    } else {
+                        return searchInsertRecursion(nums, mid, right, target);
+                    }
                 }
+            } else {
+                throw new RuntimeException();
             }
-        } else {
-            throw new RuntimeException();
         }
     }
 }
+
