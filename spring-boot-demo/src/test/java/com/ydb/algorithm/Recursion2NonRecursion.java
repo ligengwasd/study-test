ae3ad6261c51d13623d9439ee68eca143c5c5c29
李赓 <ligeng@1dabang.cn>
2018-08-19 22:44:32 +0800

    递归转非递归算法

--- spring-boot-demo/src/test/java/com/ydb/algorithm/Recursion2NonRecursion.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/Recursion2NonRecursion.java
@@ -9,26 +9,41 @@ package com.ydb.algorithm;
 public class Recursion2NonRecursion {
     public static void main(String[] args) {
         long time1 = System.currentTimeMillis();
-        System.out.println(f2(40));
+        System.out.println(f2(10));
         long time2 = System.currentTimeMillis();
-        System.out.println(f(40));
+        System.out.println(f3(10, 1,1));
         long time3 = System.currentTimeMillis();
 
-        System.out.println(time2-time1);
-        System.out.println(time3-time2);
+//        System.out.println(time2-time1);
+//        System.out.println(time3-time2);
     }
 
     //斐波那契数列
     //递归
     private static int f(int n) {
-        if (n==1 || n==0) return 1;
-        else return f(n-1)+f(n-2);
+        int res;
+        if (n==1 || n==0) {
+            res = 1;
+        } else {
+            res = f(n-1)+f(n-2);
+        }
+        System.out.print(res+" ");
+        return res;
     }
 
-
-    //非递归
+    // 递归转尾递归
+    private static int f3(int n, int s1, int s2) {
+        System.out.print(s2+" ");
+        int res;
+        if (n<2) {
+            res = s2;
+        } else {
+            res = f3(n-1, s2, s1+s2);
+        }
+        return res;
+    }
+    //递归转循环
     private static int f2(int n) {
-
         if(n<0){
             return -1;
         } else if(n<3) {
