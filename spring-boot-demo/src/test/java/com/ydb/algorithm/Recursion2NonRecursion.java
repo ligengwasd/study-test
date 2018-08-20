8e8ec85421e6df12ec4b9e3dcc3b748f5fdf4095
李赓 <ligeng@1dabang.cn>
2018-08-20 13:57:35 +0800

    递归转非递归算法

--- spring-boot-demo/src/test/java/com/ydb/algorithm/Recursion2NonRecursion.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/Recursion2NonRecursion.java
@@ -10,13 +10,17 @@ import java.util.Stack;
  */
 public class Recursion2NonRecursion {
     public static void main(String[] args) {
-        f2(20);
-        System.out.println();
-        f3(20, 1,1);
-        System.out.println();
-        f4(20);
-        System.out.println();
-        f1(20);
+        int n = 45;
+        System.out.println("n="+n);
+        long time1 = System.currentTimeMillis();
+        System.out.printf("非尾递归，返回值：%s ,时长：%s %n", f1(n), System.currentTimeMillis()-time1);
+        long time2 = System.currentTimeMillis();
+        System.out.printf("尾递归，返回值：%s ,时长：%s %n",f2(n, 1,1), System.currentTimeMillis()-time2);
+        long time3 = System.currentTimeMillis();
+        System.out.printf("迭代，返回值：%s ,时长：%s %n", f3(n),System.currentTimeMillis()-time3);
+        long time4 = System.currentTimeMillis();
+        System.out.printf("使用栈，返回值：%s ,时长：%s %n",f4(n),System.currentTimeMillis()-time4);
+
     }
 
     //斐波那契数列
@@ -28,26 +32,21 @@ public class Recursion2NonRecursion {
         } else {
             res = f1(n-1)+ f1(n-2);
         }
-        System.out.print(res+" ");
         return res;
     }
 
     // 递归转尾递归
-    private static int f3(int n, int s1, int s2) {
-        if (s1 == 1 && s2 == 1) {
-            System.out.print(s1+" ");
-        }
-        System.out.print(s2+" ");
+    private static int f2(int n, int s1, int s2) {
         int res;
         if (n<2) {
             res = s2;
         } else {
-            res = f3(n-1, s2, s1+s2);
+            res = f2(n-1, s2, s1+s2);
         }
         return res;
     }
     //递归转循环
-    private static int f2(int n) {
+    private static int f3(int n) {
         if(n<0){
             return -1;
         } else if(n<3) {
@@ -56,22 +55,17 @@ public class Recursion2NonRecursion {
             int s=0;
             int s1=1, s2=1;
             for (int i=2; i<=n; i++) {
-                System.out.print(s1+" ");
                 s=s1+s2;
                 s1=s2; // 保存f(n-2)的值
                 s2=s; //保存f(n-1)的值
-
-                if (i==n) {
-                    System.out.print(s1+" ");
-                    System.out.print(s2+" ");
-                }
             }
             return s;
         }
 
     }
 
-    private static void f4(int n) {
+    // 使用栈
+    private static int f4(int n) {
         Stack<Integer> stack = new Stack<>();
         int s1, s2;
         for (int i=0; i<=n; i++) {
@@ -84,8 +78,7 @@ public class Recursion2NonRecursion {
                 stack.push(s1+s2);
             }
         }
-        for (Integer item:stack) {
-            System.out.print(item+" ");
-        }
+
+        return stack.peek();
     }
 }
