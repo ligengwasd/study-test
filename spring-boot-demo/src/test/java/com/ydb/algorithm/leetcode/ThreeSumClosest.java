a229162fa1a5b00fecda7bba630fe676f1bf717c
李赓 <ligeng@1dabang.cn>
2018-09-01 10:32:26 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/ThreeSumClosest.java
@@ -0,0 +1,14 @@
+package com.ydb.algorithm.leetcode;
+
+/**
+ * https://leetcode.com/problems/3sum-closest/description/
+ * @Author ligeng
+ * @Date 18/8/29
+ * @Time 上午12:08
+ */
+public class ThreeSumClosest {
+    public int solution(int[] nums, int target) {
+        return -1;
+
+    }
+}
