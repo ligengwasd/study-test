639418b3bccbc062978b3e669575231902eedb15
李赓 <ligeng@1dabang.cn>
2018-09-01 16:21:49 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q110_BalancedBinaryTree.java
@@ -0,0 +1,24 @@
+package com.ydb.algorithm.leetcode;
+
+/**
+ * @Author ligeng
+ * @Date 18/9/1
+ * @Time 下午4:11
+ */
+public class Q110_BalancedBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = Q108_SortedArrayToBST.solution(new int[]{-10, -3, 0, 5, 9});
+        System.out.println(isBalanced(root));
+    }
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return (depth(root.left)-depth(root.right) <= 1) ? isBalanced(root.left) : isBalanced(root.right) ;
+    }
+    public static int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right))+1;
+    }
+
+
+
+}
