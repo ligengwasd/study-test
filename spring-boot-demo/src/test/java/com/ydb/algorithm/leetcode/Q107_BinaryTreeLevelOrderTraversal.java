b08ee694bbd5738703afd6275496a6d5587fef0c
李赓 <ligeng@1dabang.cn>
2018-09-01 15:54:10 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q107_BinaryTreeLevelOrderTraversal.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q107_BinaryTreeLevelOrderTraversal.java
@@ -56,15 +56,4 @@ public class Q107_BinaryTreeLevelOrderTraversal {
         }
         return wrapList;
     }
-
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-
-        public String toString() {
-            return val+"";
-        }
-    }
 }
---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/TreeNode.java
@@ -0,0 +1,17 @@
+package com.ydb.algorithm.leetcode;
+
+/**
+ * @Author ligeng
+ * @Date 18/9/1
+ * @Time 下午3:52
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    public String toString() {
+        return val+"";
+    }
+}
