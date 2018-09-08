962289b13eeceaf3450e784747256a0de5e083ac
李赓 <ligeng@1dabang.cn>
2018-09-08 22:53:29 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q190_ReverseBits.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q190_ReverseBits.java
@@ -7,10 +7,18 @@ package com.ydb.algorithm.leetcode;
  */
 public class Q190_ReverseBits {
     public static void main(String[] args) {
-
+        System.out.println(reverseBits(43261596));
     }
 
-    public int reverseBits(int n) {
-        return 1;
+
+
+    public static int reverseBits(int n) {
+        int res = 0;
+        for (int i=0; i<32; i++) {
+            res += n&1;
+            n >>>= 1;
+            if (i<31) res <<= 1;
+        }
+        return res;
     }
 }
