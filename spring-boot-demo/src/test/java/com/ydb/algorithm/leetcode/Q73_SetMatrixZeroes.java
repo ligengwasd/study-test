5a2fe707f7575771e468cf9322c8f8a9d0237a6e
李赓 <ligeng@1dabang.cn>
2018-09-18 00:14:50 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q73_SetMatrixZeroes.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q73_SetMatrixZeroes.java
@@ -12,11 +12,33 @@ public class Q73_SetMatrixZeroes {
     public static void main(String[] args) {
         int[][] array = new int[][]{
                 new int[]{0,1,2,0},
-                new int[]{3,4,5,2},
+                new int[]{3,0,5,2},
                 new int[]{1,3,1,5},
         };
-        setZeroes(array);
+        setZeroes2(array);
         System.out.println(1);
+    }
+
+    public static void setZeroes2(int[][] matrix) {
+        int col0 = 1;
+        int height = matrix.length;
+        int width = matrix[0].length;
+        for (int i=0; i<height; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j=0; j<width; j++) {
+                if (matrix[i][j] == 0) matrix[0][j] = matrix[i][0] = 0;
+            }
+        }
+
+
+        for (int i = height - 1; i >= 0; i--) {
+            for (int j = width - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+
+
     }
 
     public static void setZeroes(int[][] matrix) {
