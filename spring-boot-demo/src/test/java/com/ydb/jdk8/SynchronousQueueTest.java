962a4f47fcf79f3bfb833f45849afd9cbd41facf
李赓 <ligeng@enmonster.com>
2018-09-26 16:16:48 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/jdk8/SynchronousQueueTest.java
@@ -0,0 +1,93 @@
+package com.ydb.jdk8;
+
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * @Author ligeng
+ * @Date 18/9/26
+ * @Time 下午4:05
+ */
+public class SynchronousQueueTest {
+    static class SynchronousQueueProducer implements Runnable {
+
+        protected BlockingQueue<String> blockingQueue;
+        final Random random = new Random();
+
+        public SynchronousQueueProducer(BlockingQueue<String> queue) {
+            this.blockingQueue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String data = UUID.randomUUID().toString();
+                    System.out.println("生产者: " + data);
+                    blockingQueue.put(data);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    static class SynchronousQueueConsumer implements Runnable {
+
+        protected BlockingQueue<String> blockingQueue;
+
+        public SynchronousQueueConsumer(BlockingQueue<String> queue) {
+            this.blockingQueue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String data = blockingQueue.take();
+                    System.out.println(Thread.currentThread().getName()
+                            + " 消费者: " + data);
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        final BlockingQueue<String> synchronousQueue = new SynchronousQueue<>();
+
+        SynchronousQueueProducer queueProducer = new SynchronousQueueProducer(
+                synchronousQueue);
+        new Thread(queueProducer).start();
+
+        SynchronousQueueConsumer queueConsumer1 = new SynchronousQueueConsumer(
+                synchronousQueue);
+        new Thread(queueConsumer1).start();
+
+//        SynchronousQueueConsumer queueConsumer2 = new SynchronousQueueConsumer(
+//                synchronousQueue);
+//        new Thread(queueConsumer2).start();
+
+    }
+
+
+
+
+
+
+
+    @Test
+    public void test1() {
+
+    }
+}
