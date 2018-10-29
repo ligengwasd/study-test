0e3eaa492bcc8953e08f8b4382087c8b4f514202
李赓 <ligeng@enmonster.com>
2018-10-29 09:19:39 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q414_ThirdMaximumNumber.java
@@ -0,0 +1,31 @@
+package com.ydb.algorithm.leetcode;
+
+/**
+ * @Author ligeng
+ * @Date 18/10/14
+ * @Time 下午1:01
+ */
+public class Q414_ThirdMaximumNumber {
+    public static void main(String[] args) {
+
+    }
+    public int thirdMax(int[] nums) {
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+}
