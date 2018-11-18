74fc8022ac70266739e97d83b317e89696c18e91
李赓 <ligeng@enmonster.com>
2018-11-18 22:33:12 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/disno/KeyGenerator.java
@@ -0,0 +1,15 @@
+package com.ydb.disno;
+
+/**
+ * @Author ligeng
+ * @Date 18/11/18
+ * @Time 下午10:32
+ */
+public interface KeyGenerator {
+    /**
+     * 生成主键.
+     *
+     * @return 自动生成的主键
+     */
+    Number generateKey();
+}
