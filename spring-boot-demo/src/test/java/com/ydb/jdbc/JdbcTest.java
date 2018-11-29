480f95868796227c76d2952ea25bd04b5624027f
李赓 <ligeng@enmonster.com>
2018-11-29 18:21:28 +0800

    jdbc test

--- spring-boot-demo/src/test/java/com/ydb/jdbc/JdbcTest.java
+++ spring-boot-demo/src/test/java/com/ydb/jdbc/JdbcTest.java
@@ -15,11 +15,15 @@ public class JdbcTest {
             DriverManager.registerDriver( myDriver );
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/study-test", "root", "wasd");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM sys_data limit 5");
 
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM sys_data limit 5");
             while (resultSet.next()){
                 System.out.println(resultSet.getString("id"));
             }
+
+            int i = statement.executeUpdate("update sys_data set name = 'ligeng' where id = '169452'");
+            System.out.println(i);
 
 
         } catch (SQLException e) {
