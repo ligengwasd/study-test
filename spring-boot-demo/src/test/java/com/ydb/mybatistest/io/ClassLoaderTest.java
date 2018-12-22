2794901652acbfed0cfdb2ebad5de56b3b9bd016
李赓 <ligeng@enmonster.com>
2018-12-23 00:07:56 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/mybatistest/io/ClassLoaderTest.java
+++ spring-boot-demo/src/test/java/com/ydb/mybatistest/io/ClassLoaderTest.java
@@ -1,5 +1,7 @@
 package com.ydb.mybatistest.io;
 
+import org.junit.Test;
+
 import java.net.URL;
 
 /**
@@ -8,12 +10,12 @@ import java.net.URL;
  * @Time 下午11:59
  */
 public class ClassLoaderTest {
-    public static void main(String[] args) {
+    @Test
+    public void testClassLoader() {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 //        Class<Blog> loadClass = (Class<Blog>) classLoader.loadClass("com.ydb.mybatistest.pojo.Blog");
         URL url = classLoader.getResource("mybatis-config.xml");
 //        Blog test = loadClass.newInstance();
         System.out.println(url);
-
     }
 }
