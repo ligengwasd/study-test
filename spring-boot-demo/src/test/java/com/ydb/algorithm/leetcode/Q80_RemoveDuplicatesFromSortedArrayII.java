7125684e3edf6236c4224265973858d911e7e045
李赓 <ligeng@enmonster.com>
2019-01-10 17:01:03 +0800

    LeetCode Q80

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q80_RemoveDuplicatesFromSortedArrayII.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q80_RemoveDuplicatesFromSortedArrayII.java
@@ -11,7 +11,27 @@ import org.junit.Test;
 public class Q80_RemoveDuplicatesFromSortedArrayII {
     @Test
     public void test() {
-        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,1,2,3,3}));
+        System.out.println(removeDuplicates2(new int[]{0,0,1,1,1,1,2,3,3}));
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length <= 2) return nums.length;
+
+        int itemSum=0, newArrayLength=1;
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                itemSum ++;
+                if (itemSum < 2) {
+                    nums[newArrayLength++] = nums[i];
+                }
+            } else {
+                nums[newArrayLength++] = nums[i];
+                itemSum = 0;
+            }
+        }
+
+        return newArrayLength;
+
     }
 
     public int removeDuplicates(int[] nums) {
