5bbe4c51841c33b3caf0843d241afd3e173dea24
李赓 <ligeng@enmonster.com>
2019-01-11 20:38:15 +0800

    LeetCode Q6

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q6_ZigZagConversion.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q6_ZigZagConversion.java
@@ -10,25 +10,31 @@ import org.junit.Test;
 public class Q6_ZigZagConversion {
     @Test
     public void test() {
-        System.out.println(convert("PAYPALISHIRING",3));
+        System.out.println(convert("PAYPALISHIRING",2));
     }
 
-    public String convert(String s, int nRows) {
-        char[] c = s.toCharArray();
-        int len = c.length;
-        StringBuffer[] sb = new StringBuffer[nRows];
-        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
-
-        int i = 0;
-        while (i < len) {
-            for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
-                sb[idx].append(c[i++]);
-            for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
-                sb[idx].append(c[i++]);
+    public String convert(String s, int numRows) {
+        char[] chars = s.toCharArray();
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i=0; i<numRows; i++) {
+            sb[i] = new StringBuffer();
         }
-        for (int idx = 1; idx < sb.length; idx++)
-            sb[0].append(sb[idx]);
-        return sb[0].toString();
+
+        int i=0;
+        while (i<chars.length) {
+            for (int idx=0; idx<numRows && i<chars.length; idx++) {
+                sb[idx].append(chars[i++]);
+            }
+            for (int idx=numRows-2; (idx>=1)&&(i<chars.length); idx--) {
+                sb[idx].append(chars[i++]);
+            }
+        }
+
+        String res = "";
+        for (StringBuffer item:sb) {
+            res +=item.toString();
+        }
+        return res;
 
     }
 }
