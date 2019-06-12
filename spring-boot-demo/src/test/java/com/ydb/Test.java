2fd5427c89b225b3a4522c46506bf01c3f325e12
李赓 <ligeng@enmonster.com>
2019-06-13 07:56:25 +0800

    测试类

---
+++ spring-boot-demo/src/test/java/com/ydb/Test.java
@@ -0,0 +1,24 @@
+package com.ydb;
+
+/**
+ * @Author ligeng
+ * @Date 19/6/7
+ * @Time 上午10:05
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        int n=101-1;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 1;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 2;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 4;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 8;
+        System.out.println(Integer.toBinaryString(n));
+        n |= n >>> 16;
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
