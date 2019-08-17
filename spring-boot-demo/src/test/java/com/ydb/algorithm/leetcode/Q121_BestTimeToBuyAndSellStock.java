64468841bd717156577da5e3addd612d21945fa5
李赓 <ligeng@enmonster.com>
2019-08-17 15:19:43 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q121_BestTimeToBuyAndSellStock.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q121_BestTimeToBuyAndSellStock.java
@@ -1,6 +1,9 @@
 package com.ydb.algorithm.leetcode;
 
 /**
+ * 通用解法地址：
+ * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/solution/yi-ge-tong-yong-fang-fa-tuan-mie-6-dao-gu-piao-wen/#comment
+ *
  * @Author ligeng
  * @Date 18/9/2
  * @Time 下午7:23
