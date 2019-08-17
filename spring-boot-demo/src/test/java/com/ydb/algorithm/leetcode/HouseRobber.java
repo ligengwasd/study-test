afdc238d6c7365e3786b37fdc68fce69cd298932
李赓 <ligeng@enmonster.com>
2019-08-17 19:34:49 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/HouseRobber.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/HouseRobber.java
@@ -10,10 +10,30 @@ import java.util.Arrays;
  */
 public class HouseRobber {
     public static void main(String[] args) {
-        System.out.println(rob(new int[]{2,1,1,2}));
+        System.out.println(rob2(new int[]{2,1,1,2}));
     }
 
 
+    // DP数组里存储的搜索到第i栋房子的时候， 能抢到的金钱的最大值。
+    // 数组还可以进一步优化成两个变量，因为只用到了DP数组的i-1,i-2两个值。
+    public static int rob2(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+        for (int i=0; i<nums.length; i++) {
+            if (i==0) {
+                dp[i] = Math.max(0, nums[i]);
+                continue;
+            }
+            if (i==1) {
+                dp[i] = Math.max(nums[i], nums[i-1]);
+                continue;
+            }
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];
+    }
 
     // 动态规划的原始版本，根据两个变量计算，第一个变量是第i栋房子，第二个变量是抢或者不抢
     public static int rob(int[] nums) {
