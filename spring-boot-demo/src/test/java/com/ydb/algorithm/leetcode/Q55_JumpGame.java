43afd563f6c21441bd45ec35d3011308e1a17c1d
李赓 <ligeng@enmonster.com>
2019-08-17 12:31:05 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q55_JumpGame.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q55_JumpGame.java
@@ -15,7 +15,7 @@ public class Q55_JumpGame {
     public static boolean canJump(int[] nums) {
         int reach=0;
         for (int i=0; i<nums.length; i++) {
-//            if (i>reach || reach>=nums.length-1) break;
+            if (i>reach || reach>=nums.length-1) break;
             reach = Math.max(reach, i+nums[i]);
             System.out.println(reach);
         }
