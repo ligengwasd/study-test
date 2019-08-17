b9981bbe89a70e628ca2bbccb5be08505183b289
李赓 <ligeng@enmonster.com>
2019-08-17 11:39:47 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q55_JumpGame.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q55_JumpGame.java
@@ -1,6 +1,9 @@
 package com.ydb.algorithm.leetcode;
 
 /**
+ * 解释：对于节点i，有两种选择，经过节点i和不经过节点i
+ * 经过节点i：能跳到的最大位置 = i + nums[i]
+ * 不经过节点i：能跳到的最大位置  = reach
  * @Author ligeng
  * @Date 18/9/12
  * @Time 上午10:00
@@ -12,8 +15,9 @@ public class Q55_JumpGame {
     public static boolean canJump(int[] nums) {
         int reach=0;
         for (int i=0; i<nums.length; i++) {
-            if (i>reach || reach>=nums.length-1) break;
+//            if (i>reach || reach>=nums.length-1) break;
             reach = Math.max(reach, i+nums[i]);
+            System.out.println(reach);
         }
         return reach >= nums.length-1;
     }
