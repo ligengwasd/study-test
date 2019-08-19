f36b50948070ef0b50e12b1ae62633823a84a5ee
李赓 <ligeng@enmonster.com>
2019-08-19 23:55:13 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/CombinationSumIII.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/CombinationSumIII.java
@@ -11,25 +11,29 @@ import java.util.List;
 public class CombinationSumIII {
     public static void main(String[] args) {
         CombinationSumIII solution = new CombinationSumIII();
+        List<List<Integer>> lists = solution.combinationSum3(3, 9);
+        System.out.println(1);
     }
 
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
+        dfs(k, n, new ArrayList<>(), result, 1);
         return result;
     }
 
-    private void dfs(int step, int gap, List<Integer> path, List<List<Integer>> result) {
+    private void dfs(int step, int gap, List<Integer> path, List<List<Integer>> result, int start) {
         if (step == 0) {
             if (gap == 0) {
-                result.add(path);
+                result.add(new ArrayList<>(path));
             }
             return;
         }
 
-
-
-
-
-
+        for (int i=start; i<10; i++) {
+            if (gap-i <0) return;
+            path.add(i);
+            dfs(step-1, gap-i, path, result, i+1);
+            path.remove(path.size()-1);
+        }
     }
 }
