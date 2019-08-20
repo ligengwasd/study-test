2daee9391aa152efbb01507c087fd87b72799162
李赓 <ligeng@enmonster.com>
2019-08-20 16:48:30 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/InvertBinaryTree.java
@@ -0,0 +1,34 @@
+package com.ydb.algorithm.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author ligeng
+ * @Date 19/8/20
+ * @Time 下午4:41
+ */
+public class InvertBinaryTree {
+    public static void main(String[] args) {
+        InvertBinaryTree solution = new InvertBinaryTree();
+
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) q.offer(node.left);
+            if (node.right != null) q.offer(node.right);
+        }
+
+        return root;
+    }
+}
