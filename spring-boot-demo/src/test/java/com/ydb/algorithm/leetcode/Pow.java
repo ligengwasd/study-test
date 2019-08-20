56fae3a8e4fb408d785c5d4b8193cee7dd3822e1
李赓 <ligeng@enmonster.com>
2019-08-20 10:08:37 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Pow.java
@@ -0,0 +1,33 @@
+package com.ydb.algorithm.leetcode;
+
+/**
+ * @Author ligeng
+ * @Date 19/8/20
+ * @Time 上午9:56
+ */
+public class Pow {
+    public static void main(String[] args) {
+        Pow solution = new Pow();
+    }
+
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            return 1.0 / power(x, -n);
+        } else {
+            return power(x, n);
+        }
+    }
+
+    private double power(double x, int n) {
+        if (n==0) return 1;
+        if (n==1) return x;
+
+        double v = power(x, n/2);
+
+        if (n%2 == 0) {
+            return v*v;
+        } else {
+            return v*v*x;
+        }
+    }
+}
