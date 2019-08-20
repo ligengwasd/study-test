9079210b5b704de3c47c0907856241194069bda4
李赓 <ligeng@enmonster.com>
2019-08-20 17:15:56 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SameTree.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SameTree.java
@@ -14,11 +14,10 @@ public class SameTree {
        TreeNode(int x) { val = x; }
     }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // recurision method
-        if (p == null && q == null) return true;
-        if (p == null && q != null || p != null && q == null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p==null && q==null) return true;
+        if (p==null || q==null) return false;
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
     public boolean isSameTree2(TreeNode p, TreeNode q) {
