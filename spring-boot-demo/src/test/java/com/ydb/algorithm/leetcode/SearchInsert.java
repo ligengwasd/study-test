732f7610d999b6288f105b71b683341ead04f2f5
李赓 <ligeng@enmonster.com>
2019-08-21 23:28:08 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
@@ -5,7 +5,7 @@ package com.ydb.algorithm.leetcode;
  */
 public class SearchInsert {
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6};
+        int[] nums = {1, 3, 5, 6};
         int val = 4;
         SearchInsert solution = new SearchInsert();
         System.out.println(solution.searchInsert(nums, val));
@@ -14,13 +14,14 @@ public class SearchInsert {
 //        System.out.println(solution.searchInsert(nums,val));
     }
 
+    //    https://blog.csdn.net/linhuanmars/article/details/20278967
     public int searchInsert(int[] nums, int target) {
-        int l=0, r=nums.length-1;
-        while (l<=r) {
-            int mid = (l+r)/2;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
             if (target == nums[mid]) return mid;
-            if (target < nums[mid]) r = mid-1;
-            if (target > nums[mid]) l = mid+1;
+            if (target < nums[mid]) r = mid - 1;
+            if (target > nums[mid]) l = mid + 1;
         }
         return l;
     }
@@ -28,20 +29,20 @@ public class SearchInsert {
     static class Solution {
         public int searchInsert(int[] nums, int target) {
             if (nums[0] > target) return 0;
-            if (nums[nums.length-1] < target) return nums.length;
-            return searchInsertRecursion(nums,0,nums.length-1,target);
+            if (nums[nums.length - 1] < target) return nums.length;
+            return searchInsertRecursion(nums, 0, nums.length - 1, target);
         }
 
-        private int searchInsertRecursion(int[] nums,int left,int right,int target){
+        private int searchInsertRecursion(int[] nums, int left, int right, int target) {
             if (nums[left] == target) {
                 return left;
-            } else if (nums[right] == target){
+            } else if (nums[right] == target) {
                 return right;
-            } else if (nums[left] < target && target < nums[right]){
+            } else if (nums[left] < target && target < nums[right]) {
                 if (right - left == 1) {
-                    return left+1;
+                    return left + 1;
                 } else {
-                    int mid = (left+right+1)/2;
+                    int mid = (left + right + 1) / 2;
                     if (target <= nums[mid]) {
                         return searchInsertRecursion(nums, left, mid, target);
                     } else {
