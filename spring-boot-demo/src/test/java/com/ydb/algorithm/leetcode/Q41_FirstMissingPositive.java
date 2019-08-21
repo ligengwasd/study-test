52f7b0eb58a1f57c01728cc892eb54f4596b1fcf
李赓 <ligeng@enmonster.com>
2019-08-21 13:23:15 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q41_FirstMissingPositive.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q41_FirstMissingPositive.java
@@ -9,10 +9,33 @@ import java.util.Arrays;
  */
 public class Q41_FirstMissingPositive {
     public static void main(String[] args) {
-        System.out.println(firstMissingPositive(new int[]{1000,-1}));
+        Q41_FirstMissingPositive solution = new Q41_FirstMissingPositive();
+        int i = solution.firstMissingPositive(new int[]{-1, 1, 2, 0});
+        System.out.println(i);
     }
 
-    public static int firstMissingPositive(int[] nums) {
+    public int firstMissingPositive(int[] nums) {
+        bucket_sort(nums);
+        for (int i = 0; i < nums.length; ++i)
+            if (nums[i] != (i + 1))
+                return i + 1;
+        return nums.length + 1;
+    }
+    private static void bucket_sort(int[] A) {
+        final int n = A.length;
+        for (int i = 0; i < n; i++) {
+            while (A[i] != i + 1) {
+                if (A[i] < 1 || A[i] > n || A[i] == A[A[i] - 1])
+                    break;
+                // swap
+                int tmp = A[i];
+                A[i] = A[tmp - 1];
+                A[tmp - 1] = tmp;
+            }
+        }
+    }
+
+    public static int firstMissingPositive2(int[] nums) {
         if (nums.length == 0) return 1;
         Arrays.sort(nums);
         if (nums[0] > 1) return 1;
