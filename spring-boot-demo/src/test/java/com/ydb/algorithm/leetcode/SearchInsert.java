aa841609663f46ee4019fa42810b30903c52793f
李赓 <ligeng@enmonster.com>
2019-08-23 10:56:53 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/SearchInsert.java
@@ -5,7 +5,7 @@ package com.ydb.algorithm.leetcode;
  */
 public class SearchInsert {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 6};
+        int[] nums = {1, 2, 3, 5, 6, 7};
         int val = 4;
         SearchInsert solution = new SearchInsert();
         System.out.println(solution.searchInsert(nums, val));
@@ -20,8 +20,12 @@ public class SearchInsert {
         while (l <= r) {
             int mid = (l + r) / 2;
             if (target == nums[mid]) return mid;
-            if (target < nums[mid]) r = mid - 1;
-            if (target > nums[mid]) l = mid + 1;
+            if (target < nums[mid]) {
+                r = mid - 1;
+            }
+            if (target > nums[mid]) {
+                l = mid + 1;
+            }
         }
         return l;
     }
