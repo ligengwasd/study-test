4e4868ea86a9242f45ff8641d42204b2b8ad621f
李赓 <ligeng@enmonster.com>
2019-08-25 11:15:11 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/jdk8/demo/stream/Dish.java
+++ spring-boot-demo/src/test/java/com/ydb/jdk8/demo/stream/Dish.java
@@ -1,10 +1,11 @@
 package com.ydb.jdk8.demo.stream;
 
 import lombok.Data;
-import org.springframework.data.mongodb.core.mapreduce.GroupBy;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -27,6 +28,9 @@ public class Dish {
     public enum Type {
         MEAT, FISH, OTHER
     }
+    public enum CaloricLevel{
+        DIET, NORMAL, FAT
+    }
 
     public static void main(String[] args) {
         List<Dish> menu = Arrays.asList(
@@ -40,31 +44,53 @@ public class Dish {
                 new Dish("prawns", false, 300, Dish.Type.FISH),
                 new Dish("salmon", false, 450, Dish.Type.FISH));
 
-        List<String> collect = menu.stream().filter(m -> m.getCalories() > 300)
-                .map(Dish::getName)
-                .limit(3)
-                .collect(Collectors.toList());
+//        --------------------------------------收集器--------------------------------------
+        // toCollection()
+        menu.stream().collect(toCollection(ArrayList::new));
+        // counting()
+        menu.stream().collect(counting());
+        // summarizingInt()
+        menu.stream().collect(summarizingInt(Dish::getCalories));
+        // summarizingLong()
+        menu.stream().collect(summarizingLong((Dish::getCalories)));
+        // averagingInt()
+        menu.stream().collect(averagingInt(Dish::getCalories));
+        // summarizingInt()
+        IntSummaryStatistics intSummaryStatistics = menu.stream().collect(summarizingInt(Dish::getCalories));
+        // joining()
+        menu.stream().map(Dish::getName).collect(joining(","));
+        // maxBy()
+        menu.stream().collect(maxBy(Comparator.comparingInt(Dish::getCalories)));
+        // reducing()
+        menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
+        // collectingAndThen()
+        menu.stream().collect(collectingAndThen(toList(), List::size));
+        // partitioningBy()
+        menu.stream().collect(partitioningBy(Dish::isVegetarian, toSet()));
 
-        // 返回calories最大的
-        menu.stream().collect(Collectors.maxBy(Comparator.comparing(Dish::getCalories)));
-        // 分组
-        Map<Type, List<Dish>> map = menu.stream().collect(Collectors.groupingBy(Dish::getType));
-        // 多级分组
-        Map<Type, Map<Integer, List<Dish>>> mapMap = menu.stream().collect(Collectors.groupingBy(Dish::getType, Collectors.groupingBy(
-                dish -> {
-                    if (dish.getCalories() <= 400) return 0;
-                    else return 1;
-                }
-        )));
-        System.out.println(mapMap);
-        // 按子组收集数据
-        Map<Type, Long> longMap = menu.stream().collect(Collectors.groupingBy(Dish::getType, Collectors.counting()));
-        System.out.println("按子组收集数据:"+longMap);
-//        Map<Dish.Type, Optional<Dish>> mostCaloricByType =
-//                menu.stream()
-//                        .collect(groupingBy(Dish::getType,
-//                                maxBy(comparingInt(Dish::getCalories))));
-
+        // groupingBy() 取分组中有多少条数据
+        Map<Type, Long> map2 = menu.stream().collect(groupingBy(Dish::getType, counting()));
+        // groupingBy() 取分组中做大的
+        Map<Type, Optional<Dish>> map1 = menu.stream().collect(groupingBy(Dish::getType, maxBy(Comparator.comparing(Dish::getCalories))));
+        // groupingBy() 取分组中做大的，并且进一步处理去掉Optional
+        Map<Type, Dish> map3 = menu.stream().collect(
+                groupingBy(Dish::getType, collectingAndThen(maxBy(Comparator.comparing(Dish::getCalories)), Optional::get)));
+        System.out.println(map3);
+        // groupingBy() 多级分组
+        Map<Type, Map<Integer, List<Dish>>> map = menu.stream().collect(groupingBy(Dish::getType, groupingBy(dish -> {
+            if (dish.getCalories() > 500) return 1;
+            else return 0;
+        })));
+        // groupingBy联合使用的其他收集器的例子
+        Map<Dish.Type, Set<CaloricLevel>> caloricLevelsByType =
+                menu.stream().collect(
+                        groupingBy(Dish::getType, mapping(
+                                dish -> {
+                                    if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                                    else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                                    else return CaloricLevel.FAT;
+                                },
+                                toCollection(HashSet::new) )));
 
     }
 }
