951ab3297186f1855906e6e3b66755d357099e93
李赓 <ligeng@enmonster.com>
2019-08-25 15:10:11 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/jdk8/demo/defalutinterface/InterfaceDemoImpl.java
+++ spring-boot-demo/src/test/java/com/ydb/jdk8/demo/defalutinterface/InterfaceDemoImpl.java
@@ -6,4 +6,12 @@ package com.ydb.jdk8.demo.defalutinterface;
  * @Time 下午3:03
  */
 public class InterfaceDemoImpl implements InterfaceDemo {
+//    public boolean isEmpty() {
+//        return true;
+//    }
+
+    public static void main(String[] args) {
+        InterfaceDemoImpl demo = new InterfaceDemoImpl();
+        System.out.println(demo.isEmpty());
+    }
 }
