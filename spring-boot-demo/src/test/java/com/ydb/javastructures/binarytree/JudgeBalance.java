75cc3b074c89bc946be0a96f06db36782495724e
ligeng03 <ligeng03@meituan.com>
2022-12-22 20:58:06 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/javastructures/binarytree/JudgeBalance.java
@@ -0,0 +1,26 @@
+package com.ydb.javastructures.binarytree;
+
+public class JudgeBalance {
+
+    public static boolean isBalance(Node root) {
+        if (root.leftChild == null && root.rightChild == null) {
+            return true;
+        }
+        if (root.leftChild == null || root.rightChild == null) {
+            return false;
+        }
+        Integer leftDeep = getMaxDeep(root.leftChild);
+        Integer rightDeep = getMaxDeep(root.rightChild);
+        return  Math.abs(leftDeep-rightDeep) <=1 && isBalance(root.leftChild) && isBalance(root.rightChild);
+    }
+
+    public static Integer getMaxDeep(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        Integer leftDeep = getMaxDeep(node.leftChild);
+        Integer rightDeep = getMaxDeep(node.rightChild);
+        return Math.max(leftDeep, rightDeep) + 1;
+    }
+
+}
