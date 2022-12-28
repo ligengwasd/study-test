d9dd72d35cdd988afd1eee248e851a8098d87264
ligeng03 <ligeng03@meituan.com>
2022-12-28 21:01:07 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/bfs/WordLadder.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/bfs/WordLadder.java
@@ -1,56 +1,80 @@
 package com.ydb.algorithm.essentials.bfs;
 
+import com.google.common.collect.Sets;
+
 import java.util.*;
+import java.util.function.Function;
 
 public class WordLadder {
     public static void main(String[] args) {
-        int i = ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+        int i = ladderLength("hit", "cog",
+                Sets.newHashSet("hot", "dot", "dog", "lot", "log", "cog"));
         System.out.println(i);
     }
-    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (beginWord == null || endWord == null || beginWord.length() == 0 || endWord.length() == 0
-                || beginWord.length() != endWord.length())
-            return 0;
-        // 此题关键是去重，还有去除和beginWord，相同的单词
-        Set<String> set = new HashSet<String>(wordList);
-        if (set.contains(beginWord))
-            set.remove(beginWord);
-        Queue<String> wordQueue = new LinkedList<String>();
-        int level = 1; // the start string already count for 1
-        int curnum = 1;// the candidate num on current level
-        int nextnum = 0;// counter for next level
-        // 或者使用map记录层数
-        // Map<String, Integer> map = new HashMap<String, Integer>();
-        // map.put(beginWord, 1);
-        wordQueue.add(beginWord);
 
-        while (!wordQueue.isEmpty()) {
-            String word = wordQueue.poll();
-            curnum--;
-            // int curLevel = map.get(word);
-            for (int i = 0; i < word.length(); i++) {
-                char[] wordunit = word.toCharArray();
-                for (char j = 'a'; j <= 'z'; j++) {
-                    wordunit[i] = j;
-                    String temp = new String(wordunit);
-
-                    if (set.contains(temp)) {
-                        if (temp.equals(endWord))
-                            // return curLevel + 1;
-                            return level + 1;
-                        // map.put(temp, curLevel + 1);
-                        nextnum++;
-                        wordQueue.add(temp);
-                        set.remove(temp);
+    public static int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        Queue<State> q = new LinkedList<>();
+        HashSet<State> visited = new HashSet<>(); // 判重
+        final Function<State, Boolean> stateIsValid = (State s) ->
+                wordList.contains(s.word) || s.word.equals(endWord);
+        final Function<State, Boolean> stateIsTarget = (State s) ->
+                s.word.equals(endWord);
+        final Function<State, HashSet<State>> stateExtend = (State s) -> {
+            HashSet<State> result = new HashSet<>();
+            char[] array = s.word.toCharArray();
+            for (int i = 0; i < array.length; ++i) {
+                final char old = array[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    // 防止同字母替换
+                    if (c == array[i]) continue;
+                    array[i] = c;
+                    State newState = new State(new String(array), s.level + 1);
+                    if (stateIsValid.apply(newState) &&
+                            !visited.contains(newState)) {
+                        result.add(newState);
                     }
+                    array[i] = old; // 恢复该单词 }
                 }
             }
-            if (curnum == 0) {
-                curnum = nextnum;
-                nextnum = 0;
-                level++;
+            return result;
+        };
+        State startState = new State(beginWord, 0);
+        q.offer(startState);
+        visited.add(startState);
+        while (!q.isEmpty()) {
+            State state = q.poll();
+            if (stateIsTarget.apply(state)) {
+                return state.level + 1;
+            }
+            HashSet<State> newStates = stateExtend.apply(state);
+            for (State newState : newStates) {
+                q.offer(newState);
+                visited.add(newState);
             }
         }
         return 0;
+    }
+
+    static class State {
+        String word;
+        int level;
+
+        public State(String word, int level) {
+            this.word = word;
+            this.level = level;
+        }
+
+        @Override
+        public int hashCode() {
+            return word.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) return true;
+            if (this.hashCode() != other.hashCode()) return false;
+            if (!(other instanceof State)) return false;
+            return this.word.equals(((State) other).word);
+        }
     }
 }
