c09a6fa7c6f98b150231e597fd9081bbf53f0def
ligeng <914076530@qq.com>
2023-01-05 10:23:16 +0800

    Changes

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/ConstructBinaryTreeFromPreorder.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/ConstructBinaryTreeFromPreorder.java
@@ -3,45 +3,37 @@ package com.ydb.algorithm.essentials.binarytree;
 import com.ydb.algorithm.leetcode.TreeNode;
 
 public class ConstructBinaryTreeFromPreorder {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode(int x) { val = x; }
-     * }
-     */
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return preIn(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+
+    public static void main(String[] args) {
+        int[] preorder = new int[]{};
+        int[] inorder = new int[]{};
+        buildTree(preorder, inorder, 0, 0, inorder.length-1);
     }
-    public static TreeNode preIn(int[] preorder, int preLeft, int preRight, int[] inorder, int inLeft, int inRight) {
-        if (preorder.length == 0) {
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder, int rootPreorderIndex , int start, int end) {
+        if (start > end) {
             return null;
         }
         // 创建根节点
-        TreeNode root = new TreeNode(preorder[preLeft]);
-        root.left = null;
-        root.right = null;
-        // 存储左子树的长度
-        int leftLength = 0;
-        // 存储右子树的长度
-        int rightLength = 0;
-        // 寻找左右子树长度
-        for (int i = inLeft; i < inRight+1; i++) {
-            if (inorder[i] == root.val) {
-                leftLength = i- inLeft;
-                rightLength = inRight - inLeft + 1 - leftLength -1;
-                break;
-            }
-        }
-        if (leftLength > 0) {
-            root.left = preIn(preorder, preLeft+1, preLeft+1+leftLength-1, inorder, inLeft, inLeft+leftLength-1);
-        }
-        if (rightLength > 0) {
-            root.right = preIn(preorder, preLeft+1+leftLength, preRight, inorder, inLeft+leftLength-1+2, inRight);
-        }
+        TreeNode root = new TreeNode(preorder[rootPreorderIndex]);
+        // 根节点在中序数组中的位置
+        int inOrderIndex = findInorderIndex(inorder, start, end, preorder[rootPreorderIndex]);
+        // 根节点左侧的节点长度，即左子树的节点数
+        int leftTreeSize = inOrderIndex - start;
+        // 前序：根->左->右，所以根节点的下一个节点就是该根节点的左节点。左节点作为左子树的根节点，继续搜索。
+        root.left =  buildTree(preorder, inorder, rootPreorderIndex + 1, start, inOrderIndex-1);
+        // 前序：根->左->右，所以根节点的位置+左子树的长度+1，就是右节点的位置。右节点作为右子树的根节点，继续搜索。
+        root.right = buildTree(preorder, inorder, rootPreorderIndex+leftTreeSize+1, inOrderIndex+1, end);
         return root;
+    }
+
+    // 在inorder中的下标start到下标end中，寻找 key的下标
+    private static int findInorderIndex(int[] inorder, int start, int end, int key) {
+        for (int i = start; i <= end; i++) {
+            if (inorder[i] == key)
+                return i;
+        }
+        return -1;
     }
 
 }
