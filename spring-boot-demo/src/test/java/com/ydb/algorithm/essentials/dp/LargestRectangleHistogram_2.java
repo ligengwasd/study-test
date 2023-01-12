ece30daa6f98f91c5e319999fbeb7f229e3f24d6
ligeng <914076530@qq.com>
2023-01-12 10:30:19 +0800

    Changes

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/dp/LargestRectangleHistogram_2.java
@@ -0,0 +1,23 @@
+package com.ydb.algorithm.essentials.dp;
+
+import java.util.Stack;
+
+public class LargestRectangleHistogram_2 {
+    public static void main(String[] args) {
+        int[] heights = new int[] {2,1,5,6,2,3};
+        // 在原数据的头尾分别加上0
+        int[] tmp = new int[heights.length+2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+        //存的是数组下标
+        Stack<Integer> stack = new Stack<>();
+        int maxArea=0;
+        for (int i=0; i<tmp.length; i++) {
+            while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
+                int currHeight = tmp[stack.pop()];
+                maxArea = Math.max(maxArea, currHeight * (i - stack.peek()-1));
+            }
+            stack.push(i);
+        }
+        System.out.println("最大矩形面积是:" + maxArea);
+    }
+}
