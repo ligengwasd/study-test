0ffb6ef866006d3259546edd5fabe16b55231e90
ligeng <914076530@qq.com>
2023-01-12 10:36:21 +0800

    Changes

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/dp/LargestRectangleHistogram_2.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/dp/LargestRectangleHistogram_2.java
@@ -8,10 +8,11 @@ public class LargestRectangleHistogram_2 {
         // 在原数据的头尾分别加上0
         int[] tmp = new int[heights.length+2];
         System.arraycopy(heights, 0, tmp, 1, heights.length);
-        //存的是数组下标
+        //存的是数组下标，单调递增栈
         Stack<Integer> stack = new Stack<>();
         int maxArea=0;
         for (int i=0; i<tmp.length; i++) {
+            // 当第i个元素小于栈顶元素时，计算以栈顶元素为高的矩形面积
             while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
                 int currHeight = tmp[stack.pop()];
                 maxArea = Math.max(maxArea, currHeight * (i - stack.peek()-1));
