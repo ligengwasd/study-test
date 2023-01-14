7532b012ee74ea3216830ce66d0c6d87f6d3f72c
ligeng <914076530@qq.com>
2023-01-14 14:22:07 +0800

    Changes

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/dp/EditDistance.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/dp/EditDistance.java
@@ -8,6 +8,22 @@ public class EditDistance {
         int n = word1.length();
         int m = word2.length();
         int[][] dp = new int[n + 1][m + 1];
-
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;// 这一步表示由一个空串变成pSource的编辑距离
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // 表示当前两个字符串最后的两个元素相等的状况
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
