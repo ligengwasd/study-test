c0d40983150a8172820cb18df7291220814ae4ff
geng.li <geng.li@timschina.com>
2023-02-06 16:46:02 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/FlattenBinaryTreeLinkedList.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/FlattenBinaryTreeLinkedList.java
@@ -9,13 +9,13 @@ import com.ydb.algorithm.leetcode.TreeNode;
 public class FlattenBinaryTreeLinkedList {
 
     public void flatten(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return;
         }
-        if(root.left != null){
+        if (root.left != null) {
             flatten(root.left);
         }
-        if(root.right != null){
+        if (root.right != null) {
             flatten(root.right);
         }
         // 1.记录下右子树
@@ -25,7 +25,7 @@ public class FlattenBinaryTreeLinkedList {
         // 3.左节点置空
         root.left = null;
         // 4.root指针移到最右的节点上
-        while(root.right != null){
+        while (root.right != null) {
             root = root.right;
         }
         // 5.老的右子树挂到最右的节点上
