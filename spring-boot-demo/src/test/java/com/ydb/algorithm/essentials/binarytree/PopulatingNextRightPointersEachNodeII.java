d8076b23445687bc65ce759c30b98b25b3a59e7e
geng.li <geng.li@timschina.com>
2023-02-06 17:11:50 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/PopulatingNextRightPointersEachNodeII.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/PopulatingNextRightPointersEachNodeII.java
@@ -2,4 +2,9 @@ package com.ydb.algorithm.essentials.binarytree;
 
 public class PopulatingNextRightPointersEachNodeII {
 
+    public void connect(TreeLinkNode root) {
+        while (root !=null ) {
+
+        }
+    }
 }
---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/TreeLinkNode.java
@@ -0,0 +1,11 @@
+package com.ydb.algorithm.essentials.binarytree;
+
+import com.ydb.algorithm.leetcode.TreeNode;
+
+public class TreeLinkNode extends TreeNode {
+    TreeNode next;
+
+    public TreeLinkNode(Integer x) {
+        super(x);
+    }
+}
