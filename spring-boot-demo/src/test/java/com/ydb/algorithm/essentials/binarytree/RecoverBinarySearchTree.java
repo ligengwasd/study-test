e5c550dd5cd4adf5bc01634d256889f982bc219d
geng.li <geng.li@timschina.com>
2023-02-06 15:30:41 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/RecoverBinarySearchTree.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/RecoverBinarySearchTree.java
@@ -1,7 +1,22 @@
 package com.ydb.algorithm.essentials.binarytree;
 
+import com.ydb.algorithm.leetcode.TreeNode;
+
 public class RecoverBinarySearchTree {
+    private TreeNode pre;
+    private TreeNode first;
+    private TreeNode second;
     public static void main(String[] args) {
-        System.out.println(1);
+
     }
+
+    void inOrder(TreeNode root) {
+        inOrder(root.left);
+        if (pre != null && pre.val > root.val) {
+
+        }
+        inOrder(root.right);
+    }
+
+
 }
