edc2a69d4a72934eab9da17c2d07a94fa8ee2dc2
geng.li <geng.li@timschina.com>
2023-02-06 16:34:38 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/FlattenBinaryTreeLinkedList.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/FlattenBinaryTreeLinkedList.java
@@ -1,5 +1,26 @@
 package com.ydb.algorithm.essentials.binarytree;
 
+import com.ydb.algorithm.leetcode.TreeNode;
+
 public class FlattenBinaryTreeLinkedList {
 
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        if(root.left != null){
+            flatten(root.left);
+        }
+        if(root.right != null){
+            flatten(root.right);
+        }
+
+        TreeNode node = root.right;
+        root.right = root.left;
+        root.left = null;
+        while(root.right != null){
+            root = root.right;
+        }
+        root.right = node;
+    }
 }
