541017413f1d8ec65d92a0bc1915ebd36557437d
ligeng <914076530@qq.com>
2023-02-07 23:04:58 +0800

    Changes

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/recursion/PathSumII.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/recursion/PathSumII.java
@@ -7,7 +7,18 @@ import java.util.List;
 
 public class PathSumII {
     public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
 
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+
+        PathSumII pathSumII = new PathSumII();
+        List<List<Integer>> lists = pathSumII.pathSum(treeNode1, 7);
+        System.out.println(1);
     }
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
