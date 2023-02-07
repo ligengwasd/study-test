b3bf4004ac8be71ef748a51ae7d06a967aca7f4c
geng.li <geng.li@timschina.com>
2023-02-07 15:21:26 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/search/ValidateBinarySearchTree.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/search/ValidateBinarySearchTree.java
@@ -7,9 +7,9 @@ public class ValidateBinarySearchTree {
 
     }
 
-    public static boolean isValidBSF(TreeNode root, TreeNode left, TreeNode right) {
-        return root.val > left.val && root.val< right.val
-                && isValidBSF(left, left.left, left.right)
-                && isValidBSF(right, right.left, right.right);
+    public static boolean isValidBSF(TreeNode root) {
+        return root.val > root.left.val && root.val< root.right.val
+                && isValidBSF(root.left)
+                && isValidBSF(root.right);
     }
 }
