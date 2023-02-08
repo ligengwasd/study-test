f28d0263b980c8789e349a71fa14da3bcad81611
ligeng <914076530@qq.com>
2023-02-08 21:32:36 +0800

    Changes

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/recursion/LCABinaryTree.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/recursion/LCABinaryTree.java
@@ -4,6 +4,22 @@ import com.ydb.algorithm.leetcode.TreeNode;
 
 public class LCABinaryTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            // 走到这里，说明p和q正好在上一层的root节点的左右两边。
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left !=null && right !=null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
     }
 }
