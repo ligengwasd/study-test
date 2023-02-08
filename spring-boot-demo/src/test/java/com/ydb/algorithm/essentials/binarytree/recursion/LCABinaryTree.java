d6c86bd6c263150c2ec93fe9340f260fda75b39a
geng.li <geng.li@timschina.com>
2023-02-08 20:31:10 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/binarytree/recursion/LCABinaryTree.java
@@ -0,0 +1,9 @@
+package com.ydb.algorithm.essentials.binarytree.recursion;
+
+import com.ydb.algorithm.leetcode.TreeNode;
+
+public class LCABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
