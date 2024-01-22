e82b255b12b17615044955b5fe7728bc3b7e8375
geng.li <geng.li@timschina.com>
2024-01-22 12:37:42 +0800

    update

---
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q139_WordBreak.java
@@ -0,0 +1,36 @@
+package com.ydb.algorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q139_WordBreak {
+    public static void main(String[] args) {
+        Q139_WordBreak q = new Q139_WordBreak();
+        q.wordBreak("abcd", Arrays.asList("a","abc","b","cd"));
+        System.out.println(q.res);
+    }
+
+    boolean res = false;
+    boolean wordBreak(String s, List<String> wordDict) {
+        wordBreakInner(s, wordDict, 0);
+        return res;
+    }
+
+    void wordBreakInner(String s, List<String> wordDict, int start) {
+        if (res) {
+            return;
+        }
+        if (start == s.length()) {
+            res = true;
+            return;
+        }
+        for (String word : wordDict) {
+            if (start + word.length() > s.length()) {
+                continue;
+            }
+            if (s.substring(start, start + word.length()).equals(word)) {
+                wordBreakInner(s, wordDict, start + word.length());
+            }
+        }
+    }
+}
