480dc6358576f92f0161210ae3bd7409a4101313
geng.li <geng.li@timschina.com>
2024-01-23 10:34:50 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Bag.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Bag.java
@@ -16,25 +16,38 @@ public class Bag {
         System.out.println(knapsack);
     }
 
-    int knapsack(int W, int v, int[] wt, int[] val) {
-        // base case 已初始化
-        int[][] dp = new int[v + 1][W + 1];
-        for (int i = 1; i <= v; i++) {
-            for (int w = 1; w <= W; w++) {
-                if (w - wt[i - 1] < 0) {
-                    // 这种情况下只能选择不装入背包
-                    dp[i][w] = dp[i - 1][w];
-                } else {
-                    // 装入或者不装入背包，择优
+    /**
+     *
+     * @param W 背包重量上限
+     * @param I 物品数量
+     * @param wt
+     * @param val
+     * @return
+     */
+    int knapsack(int W, int I, int[] wt, int[] val) {
+        // base case 已初始化。使用W+1，是为了让数组从1开始，这样数组下标就是重量
+        int[][] dp = new int[I][W + 1];
+        // i = 0的时候，即决策第1个商品的时候，只有1个商品，放的下就放，放不下就是0
+        for (int w = 1; w < W + 1; w++) {
+            if (w > wt[0]) {
+                dp[0][w] = val[0];
+            } else {
+                dp[0][w] = 0;
+            }
+        }
+        for (int i = 1; i < I; i++) {
+            for (int w = 1; w < W + 1; w++) {
+                if (w-wt[i] >0) { // 重量上限大于第i个物品的重量，说明可以选择该物品，并且选择后剩余重量（w-wt[i]）用来装其它物品，即dp[i-1][w - wt[i]]
                     dp[i][w] = Math.max(
-                            dp[i - 1][w - wt[i-1]] + val[i-1],
-                            dp[i - 1][w]
+                            dp[i-1][w],  // 不选择第i个物品
+                            dp[i-1][w - wt[i]] + val[i] // 选择第i个物品
                     );
+                } else {// 第i个物品超过了重量上限，只能不选择该物品。
+                    dp[i][w] = dp[i-1][w];
                 }
             }
         }
-
-        return dp[v][W];
+        return dp[I-1][W];
     }
 
     void knapsack(int[] wt, int[] val, int maxWeight, int curr, boolean[] selected) {
