91e25cced847f0e697a271624ed8101f7a1fb581
geng.li <geng.li@timschina.com>
2024-01-31 10:37:02 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/greed/Q55_JumpGame.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/essentials/greed/Q55_JumpGame.java
@@ -7,16 +7,20 @@ public class Q55_JumpGame {
         System.out.println(canJump(new int[]{2,3,1,1,4}));
     }
     public static boolean canJump(int[] nums) {
-        int reach=1;
-        // i从1开始，i表示第i-1个节点
-        for (int i = 1; i <= nums.length; i++) {
-            // 最大能到的距离小于i（i是从0开始的距离），说明无法走到节点i
-            if (i > reach) {
+        // 从第0个元素出发，能到达的最远距离
+        int reach = 1;
+        // 到达第0个元素，需要0步
+        // 到达第1个元素，需要1步
+        // 到达第i个元素，需要i步
+        for (int i = 0; i < nums.length; i++) {
+            // reach表示能到达的最远距离，如果reach刚好等于i，那么走到第i个元素的时候，剩余步数是0，无法前进。
+            if (i >= reach) {
                 return false;
             }
-            reach = Math.max(reach, i + nums[i-1]);
+            // 从第i个元素触发，能到达的最远距离是i + nums[i] + 1，
+            // 注意reach表达的是距离，从第0个元素触发，能到的最远距离是 1+nums[0]，所以需要加1
+            reach = Math.max(reach, i + nums[i] + 1);
         }
-        return reach >= nums.length;
-
+        return reach > nums.length-1;
     }
 }
