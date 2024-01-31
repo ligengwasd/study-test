d3b6fd0c9e6e33a5127d91dde4bcafb8748dec77
geng.li <geng.li@timschina.com>
2024-01-31 17:02:12 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q132_PalindromePartitioningII.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/Q132_PalindromePartitioningII.java
@@ -5,7 +5,10 @@ public class Q132_PalindromePartitioningII {
         char[] charArray = s.toCharArray();
         int n = charArray.length;
         boolean[][] dp = new boolean[n][n];
+        int[] res = new int[n];
         for (int i = 0; i < n; i++) {
+            // 对于前i个字符，最差的情况，每个字母分割一次（单个字母也是回文），需要i次分割
+            res[i] = i;
             for (int j = 0; j <= i; j++) {
                 if (charArray[i] == charArray[j] && (j -i < 2) || dp[i+1][j-1]) {
                     dp[i][j] = true;
