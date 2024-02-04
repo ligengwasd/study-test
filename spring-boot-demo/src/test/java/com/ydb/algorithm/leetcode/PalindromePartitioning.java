c992800c96450081e60239722a8f1efd8c7f8c02
geng.li <geng.li@timschina.com>
2024-02-04 20:38:17 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/PalindromePartitioning.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/PalindromePartitioning.java
@@ -1,5 +1,7 @@
 package com.ydb.algorithm.leetcode;
 
+import com.google.gson.Gson;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,16 +19,24 @@ public class PalindromePartitioning {
      * @param end 是否在end的地方断开
      */
     public void dfs(LinkedList<String> path, String s, int start) {
+        if (start == s.length()) {
+            System.out.println(new Gson().toJson(path));
+        }
         for (int i = start; i < s.length(); i++) {
             if (isPalindrome(s, start, i)) {
-                path.add(s.substring(start, i));
+                path.add(s.substring(start, i+1));
                 dfs(path, s, i+1);
                 path.removeLast();
             }
         }
     }
-
-
+    /**
+     * 左闭右闭区间
+     * @param s
+     * @param left
+     * @param right
+     * @return
+     */
     private boolean isPalindrome(String s, int left, int right) {
         while (right - left > 1) {
             if (s.charAt(left) != s.charAt(right)) {
