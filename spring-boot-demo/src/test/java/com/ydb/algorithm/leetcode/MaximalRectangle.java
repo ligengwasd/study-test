b09c2e6b25de6e5f17dfea850c4a13f984813e74
geng.li <geng.li@timschina.com>
2024-02-04 23:35:00 +0800

    update

--- spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/MaximalRectangle.java
+++ spring-boot-demo/src/test/java/com/ydb/algorithm/leetcode/MaximalRectangle.java
@@ -13,6 +13,7 @@ public class MaximalRectangle {
         int[][] heightsDp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                // 如果当前元素是0，高度就是0，不需要处理
                 if (matrix[i][j] == 1) {
                     int preHeight = i == 0 ? 0 : heightsDp[i-1][j];
                     heightsDp[i][j] = preHeight + 1;
